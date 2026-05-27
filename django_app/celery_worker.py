6240d599cb9e534117cb697605d4a60be3ee6b5a
kushal-pandey <kushalpandey10january@gmail.com>
2026-05-27 15:31:39 +0530

    Change cronjob URL

--- django_app/celery_worker.py
+++ django_app/celery_worker.py
@@ -1,11 +1,19 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('devboard')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+app.conf.beat_schedule = {
+    'keep-services-alive': {
+        'task': 'apps.accounts.tasks.ping_services',
+        'schedule': crontab(minute='*/10'),  
+    },
+}
 
 @app.task(bind=True)
 def debug_task(self):
